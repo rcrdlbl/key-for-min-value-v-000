561ce11bacc63916593ca321f3f967109bcad67b
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-04 04:34:10 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,5 +6,13 @@ def key_for_min_value(name_hash)
     nil
   else
     lowest_key = name_hash.first[0]
-    lowest_key = name_hash.first[1]
+    lowest_value = name_hash.first[1]
+    name_hash.each do |key, value|
+      if value < lowest_value
+        lowest_key = key
+        lowest_value = value
+      end
+    end
+    lowest_key
+  end
 end
